f53355fa39f1c813e187e67a597144529bc51a71
Mert Ateş Topuz <m.atestopuz@gmail.com>
2023-08-17 21:54:38 +0300

    Commit 8
    8

---
+++ My project/Assets/Script/TimeManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.Rendering.Universal;
+
+public class TimeManager : MonoBehaviour
+{
+    public static int Minute { get; private set; }
+    public static int Hour { get; private set; }
+    public static int LightI { get; private set; }
+
+    private float minuteToRealTime = 1f;
+    private float timer;
+
+    public Light2D mainLight;
+    private bool lightTimer;
+
+    [Header("UI")]
+    public Image img;
+    public TextMeshProUGUI txtPro;
+
+    [Header("UIType")]
+    public Sprite day;
+    public Sprite night;
+    public Color dayColor;
+    public Color nightColor;
+
+    void Start()
+    {
+        img.sprite = day;
+        txtPro.color = dayColor;
+
+        Minute = 0;
+        Hour = 10;
+        timer = minuteToRealTime;
+    }
+
+    void Update()
+    {
+        MainTimer();
+        txtPro.text = Hour.ToString()+ ":" + Minute.ToString("00");
+        
+        ChangeSprite();
+    }
+
+    private void MainTimer()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Minute++;
+            if (lightTimer == true)
+            {
+                LightUpdater();
+            }
+            
+            if (Minute > 60)
+            {
+                Hour++;
+                Minute = 0;
+            }
+            timer = minuteToRealTime;
+        }
+    }
+
+   private void ChangeSprite()
+    {
+        if (Hour == 10 && Minute == 10)
+        {
+            img.sprite = night;
+            txtPro.color = nightColor;
+            lightTimer = true;
+            
+        }
+    }
+    private void LightUpdater()
+    {
+            mainLight.intensity -= 0.1f;
+        
+            if (mainLight.intensity <= 0.05f)
+            {
+                mainLight.intensity = 0.05f;
+            }
+    }
+}
